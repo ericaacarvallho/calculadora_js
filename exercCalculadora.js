380647a9ed19802a860dee62a4903762eefa4f7b
ericaacarvalho <ericaemiliano@hotmail.com>
2024-08-29 19:43:57 -0300

    calculadora

---
+++ exercCalculadora.js
@@ -0,0 +1,12 @@
+export function exercCalculadora(operand1, operand2, operator) {
+    switch (operator) {
+        case '+':
+            return parseInt(operand1) + parseInt(operand2);
+        case '-':
+            return operand1 - operand2;
+        case '*':
+            return operand1 * operand2;
+        case '/':
+            return operand1 / operand2;
+    }
+}
---
+++ pritnCalculadora.js
@@ -0,0 +1,13 @@
+import { exercCalculadora } from "./exercCalculadora.js";
+
+console.log(exercCalculadora(1, 1, "+"));
+console.log(2);
+
+console.log(exercCalculadora(1, 1, "-"));
+console.log(0);
+
+console.log(exercCalculadora(1, 1, "*"));
+console.log(1);
+
+console.log(exercCalculadora(1, 1, "/"));
+console.log(1);
